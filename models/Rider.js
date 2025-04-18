f47c844106b8cced2cedcc644a173fc337f96099
Deepanshi <deepanshik825@gmail.com>
2025-04-19 00:38:09 +0530

    restructing

--- models/Rider.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const riderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', default: null }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Rider', riderSchema);
